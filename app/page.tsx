d5d485787e085703cf8750e4427ebf1c1a640450
lisonallen <lisonallen@qq.com>
2023-12-29 15:35:12 +0800

    search bar init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Header from '../components/header'
 
 export default function Home() {
@@ -7,103 +6,28 @@ export default function Home() {
             <Header/>
 
             {/* Search Box */}
-            <div className="w-full max-w-2xl">
-                <div className="relative flex items-center rounded-full shadow-lg">
+            <div className="w-full max-w-4xl mb-36">
+                <div className="relative flex items-center rounded-xl border border-gray-100 bg-gray-100 p-4">
                     <input
                         type="text"
-                        className="w-full rounded-full py-4 pl-10 pr-20 text-lg border-none focus:ring-0"
+                        className="w-full py-4 pl-10 pr-10 mr-16 text-lg border-none focus:ring-0 focus:outline-none  bg-gray-100"
                         placeholder="请输入搜索内容..."
                     />
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 6H6v12h2V6zM16 6h-2v12h2V6z" />
-                        </svg>
-                    </div>
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-14">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12H9m6 0H9m6 0H9" />
-                        </svg>
-                    </div>
-                    <button className="absolute inset-y-0 right-0 flex items-center rounded-r-full bg-gray-200 px-6 font-bold">
+                    <button className="absolute inset-y-0 right-0 flex items-center rounded-r-xl border-l border-gray-300 bg-gray-200 px-6 font-bold">
                         →
                     </button>
                 </div>
             </div>
 
-            <div className="mb-32 grid text-center lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            <div className="mb-32 text-center lg:w-full lg:mb-0 lg:text-center">
                 <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    href="mailto:lisonallen@qq.com"
+                    className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:underline"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Code Insight{' '}
-                        <span
-                            className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Harnessing AI to delve deep into the world of programming, providing accurate and rapid code
-                        analysis and solutions for developers.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Smart Search{' '}
-                        <span
-                            className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Integrating artificial intelligence to inspire developers&apos; creativity, expanding
-                        programming horizons and enhancing search efficiency and innovation.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Code Answer{' '}
-                        <span
-                            className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Offering quick and accurate answers to programming queries, making coding information and
-                        solutions easily accessible, aiding developers in solving problems efficiently.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        EffiExplore{' '}
-                        <span
-                            className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Accelerating the exploration of programming knowledge, improving learning and work efficiency,
-                        and guiding developers towards a path of success.
+                    <p className={`m-0 text-sm opacity-50`}>
+                        &copy;Noodlion
                     </p>
                 </a>
             </div>
