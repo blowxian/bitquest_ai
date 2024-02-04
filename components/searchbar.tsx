c5711897fdfdac6b27618726a4bc80f4dcccc151
lisonallen <lisonallen@qq.com>
2024-02-04 11:34:11 +0800

    fix search input shortcut;

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -3,7 +3,7 @@
 import {useSearchParams, useRouter} from 'next/navigation';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 
 export default function SearchBar() {
     const searchParams = useSearchParams();
@@ -24,6 +24,31 @@ export default function SearchBar() {
             router.push(`/search?${params.toString()}`);
         }
     }
+
+    // 键盘事件处理函数
+    const handleGlobalKeyDown = (event: KeyboardEvent) => {
+
+        // 检查是否按下了 'K' 键，同时按下了 'Meta' 键（Mac）或 'Control' 键（Windows/Linux）
+        if ((event.metaKey || event.ctrlKey) && event.key === 'k') {
+            event.preventDefault(); // 阻止默认行为
+
+            const searchInput = inputRef.current;
+            if (searchInput) {
+                searchInput.focus(); // 聚焦到输入框
+                searchInput.select(); // 全选输入框内容
+            }
+        }
+    };
+
+    useEffect(() => {
+        // 添加事件监听器
+        window.addEventListener('keydown', handleGlobalKeyDown);
+
+        // 组件卸载时移除事件监听器
+        return () => {
+            window.removeEventListener('keydown', handleGlobalKeyDown);
+        };
+    }, []);
 
     return (
         <div className={"w-3/4 max-w-6xl"}>
