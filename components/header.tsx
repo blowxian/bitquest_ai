3dd64a9f428e8b9172f27ee700d7ed886f815f22
lisonallen <lisonallen@qq.com>
2024-02-04 11:25:01 +0800

    add slogan below logo;

--- components/header.tsx
+++ components/header.tsx
@@ -3,23 +3,20 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="pb-6 w-full mt-96">
-            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <nav className="flex justify-center items-center h-20">
-                    <div className="">
-                        <a href="/" title="">
-                            <Image
-                                className="w-auto h-16"
-                                src="/logo_final_v.svg"
-                                width={140}
-                                height={27}
-                                alt=""
-                                priority
-                            />
-                        </a>
-                    </div>
-                </nav>
+        <header className="pb-6 w-full mt-96 flex flex-col justify-center items-center">
+            <div className="px-4 sm:px-6 lg:px-8 h-20">
+                <a href="/" title="Make AI Search Affordable For Everyone,Everywhere">
+                    <Image
+                        className="w-auto h-16"
+                        src="/logo_final_v.svg"
+                        width={140}
+                        height={27}
+                        alt=""
+                        priority
+                    />
+                </a>
             </div>
+            <p className="text-center text-gray-400 text-sm">Make AI Search Affordable For Everyone,Everywhere</p>
         </header>
     )
 }
