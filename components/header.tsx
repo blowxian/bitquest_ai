a5a9ecfe86cc934e696b0b816fc20b9fba8eaab9
lisonallen <lisonallen@qq.com>
2024-02-13 12:11:41 +0800

    adjust mobile css;

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="pb-6 w-full mt-72 sm:mt-96 flex flex-col items-center">
+        <header className="pb-6 w-full mt-52 sm:mt-96 flex flex-col items-center">
             <div className="px-4 sm:px-8 h-20">
                 <a href="/" title="Make AI Search Affordable For Everyone,Everywhere">
                     <Image
-                        className="w-auto h-14 sm:h-16"
+                        className="w-auto h-12 sm:h-16"
                         src="/logo_final_v.svg"
                         width={140}
                         height={27}
