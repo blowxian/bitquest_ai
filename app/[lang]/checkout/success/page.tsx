b2786080dcd8f406b98798f09012718e8ba13acc
lisonallen <lisonallen@qq.com>
2024-05-30 22:31:55 +0800

    opt payment success info;

--- app/[lang]/checkout/success/page.tsx
+++ app/[lang]/checkout/success/page.tsx
@@ -1,10 +1,10 @@
 // /app/[lang]/checkout/success/page.tsx
 'use client'
 
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import React, {useEffect, useState} from 'react';
+import {useSearchParams} from 'next/navigation';
 import Header from '@/components/Header';
-import { Dictionary, getDictionary } from '@/app/[lang]/dictionaries';
+import {Dictionary, getDictionary} from '@/app/[lang]/dictionaries';
 
 interface SuccessPageProps {
     params: {
@@ -16,7 +16,7 @@ const fetchDictionary = async (lang: string) => {
     return await getDictionary(lang);
 }
 
-const SuccessPage: React.FC<SuccessPageProps> = ({ params }) => {
+const SuccessPage: React.FC<SuccessPageProps> = ({params}) => {
     const [dict, setDict] = useState<Dictionary | null>(null);
     const searchParams = useSearchParams();
     const sessionId = searchParams?.get('session_id');
@@ -38,10 +38,26 @@ const SuccessPage: React.FC<SuccessPageProps> = ({ params }) => {
 
     return (
         <main className="flex flex-col items-center justify-center min-h-screen w-full">
-            <Header headerDict={dict?.header} />
-            <div className="text-center">
-                <h1>Payment Successful</h1>
-                <p>Thank you for your payment!</p>
+            <div className="max-w-md w-full space-y-8">
+                <Header headerDict={dict?.header}/>
+                <div className="text-center">
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Payment Successful</h2>
+                    <p className="mt-2 text-center text-sm text-gray-600">
+                        Thank you for your payment. Your transaction has been successfully processed.
+                    </p>
+                </div>
+                <div className="mt-8 space-y-6">
+                    <p className="text-center text-sm text-gray-600">
+                        To begin your journey with Coogle, please join our Discord community by clicking the link
+                        below.
+                    </p>
+                    <div className="flex justify-center">
+                        <a href="https://discord.gg/GNv3qbm6" target="_blank"
+                           className="group relative w-64 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Join Our Discord
+                        </a>
+                    </div>
+                </div>
             </div>
         </main>
     );
