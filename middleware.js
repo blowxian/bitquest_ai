d04c6e58fe13af2086496aa40490bddbd1fc0a68
lisonallen <lisonallen@qq.com>
2024-06-05 15:06:16 +0800

    add sentry.io;

--- middleware.js
+++ middleware.js
@@ -33,7 +33,7 @@ export function middleware(request) {
 export const config = {
     matcher: [
         // Skip all internal paths (_next)
-        '/((?!_next|api|img|favicon).*)',
+        '/((?!_next|api|sentry-example-page|sentry-example-api|img|favicon).*)',
         // Optional: only run on root (/) URL
         // '/'
     ],
